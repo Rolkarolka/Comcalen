da22d1f3e3265eefb7a9cddde4d19d68ef8f7c18
Karolina Romanowska <rokarolka@outlook.com>
2020-06-04 11:22:46 +0200

    to już jest koniec (poprawa wyswietlania tabeli w kalendarzu pracodawcy)

--- Comcalen/EmployerCalendar.cpp
+++ Comcalen/EmployerCalendar.cpp
@@ -27,18 +27,19 @@ void EmployerCalendar::calendar_clicked()
 {
 	ui.employees->setRowCount(0);
 	ui.employees->setColumnCount(1);
-	
+	ui.employees->setHorizontalHeaderLabels(QStringList() << "Employees:");
+	ui.employees->horizontalHeader()->setStretchLastSection(true);
 	vector<QString> shifts = company->get_hours_employer(ui.calendar_employer->selectedDate());
 	ui.hours->setRowCount(shifts.size());
 	ui.hours->setColumnCount(1);
 	ui.hours->setHorizontalHeaderLabels(QStringList() << "Shifts:");
-	ui.hours->horizontalHeader()->setStretchLastSection(true);
 
 	for (int row = 0; row < shifts.size(); row++)
 	{
 		QTableWidgetItem* newItem = new QTableWidgetItem(shifts[row]);
 		ui.hours->setItem(row, 0, newItem);
 	}
+	ui.hours->horizontalHeader()->setStretchLastSection(true);
 }
 
 void EmployerCalendar::show_emplo(int, int)
@@ -46,12 +47,12 @@ void EmployerCalendar::show_emplo(int, int)
 	vector <string> emplo = company->get_shifts_employees(ui.calendar_employer->selectedDate(), ui.hours->currentItem()->text());
 	ui.employees->setRowCount(emplo.size());
 	ui.employees->setColumnCount(1);
-	ui.employees->setHorizontalHeaderLabels(QStringList() << "Employees:");
-	ui.employees->horizontalHeader()->setStretchLastSection(true);
 	if (emplo.size() > 0)
 		for (int row = 0; row < emplo.size(); row++)
 		{
 			QTableWidgetItem* newItem = new QTableWidgetItem(QString::fromStdString(emplo[row]));
 			ui.employees->setItem(row, 0, newItem);
-		}	
+		}
+	ui.employees->setHorizontalHeaderLabels(QStringList() << "Employees:");
+	ui.employees->horizontalHeader()->setStretchLastSection(true);
 }
